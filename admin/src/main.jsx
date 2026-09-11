f01fae675131869edbb1302bbb2009601f536fe2
Sagaruk06 <ss6604441@gmail.com>
2026-09-11 13:08:23 +0530

    Fix admin router basename and admin SPA routing

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -5,9 +5,7 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename={import.meta.env.BASE_URL}>
+  <BrowserRouter basename="/admin">
     <App />
   </BrowserRouter>
-
-
 )
